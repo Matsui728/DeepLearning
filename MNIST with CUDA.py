a22af9ff852e621d6a95d954b1561df24a6769cf
Matsui <rr0055xx@ed.ritsumei.ac.jp>
2017-06-12 15:57:02 +0900

    cupy実装済み 細かい部分の修正

--- MNIST with CUDA.py
+++ MNIST with CUDA.py
@@ -21,7 +21,7 @@ from chainer.dataset import concat_examples
 
 
 parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-parser.add_argument('--gpu', '-g', default= 0, type=int,
+parser.add_argument('--gpu', '-g', default=-0, type=int,
                     help='GPU ID (negative value indicates CPU)')
 args = parser.parse_args()
 if args.gpu >= 0:
@@ -29,21 +29,21 @@ if args.gpu >= 0:
 xp = cuda.cupy if args.gpu >= 0 else np
 
 
-
-def load_Mnist():
+def load_mnist():
     train, test = get_mnist(ndim=3)
     train = concat_examples(train)
     test = concat_examples(test)
     return train, test
 
+
 class ConvNet(chainer.Chain):
     def __init__(self):
-        super(ConvNet,self).__init__(
-            conv1 = L.Convolution2D(1, 10, 3),     #28 ->26
-            conv2 = L.Convolution2D(10, 10, 4),    #13 ->10
-            fc1 = L.Linear(250,10)
+        super(ConvNet, self).__init__(
+            conv1=L.Convolution2D(1, 10, 3),     # 28 ->26
+            conv2=L.Convolution2D(10, 10, 4),    # 13 ->10
+            fc1=L.Linear(250, 10)
         )
-        
+
     def __call__(self, x):
         h = self.conv1(x)
         h = F.max_pooling_2d(h, 2)
@@ -51,23 +51,22 @@ class ConvNet(chainer.Chain):
         h = F.max_pooling_2d(h, 2)
         y = self.fc1(h)
         return y
-       
-        
-        
+
+
 if __name__ == '__main__':
-    #ハイパーパラメータ
-    num_epochs = 10        #エポック数
-    batch_size = 500        #バッチ数
-    learing_rate = 0.001   #学習率
-    
-    #データ読み込み
-    train, test = load_Mnist()
+    # ハイパーパラメータ
+    num_epochs = 10        # エポック数
+    batch_size = 500        # バッチ数
+    learing_rate = 0.001   # 学習率
+
+    # データ読み込み
+    train, test = load_mnist()
     x_train, c_train = train
     x_test, c_test = test
     num_train = len(x_train)
     num_test = len(x_test)
 
-    #モデル、オプティマイザ（chainer関数の使用）
+    # モデル、オプティマイザ（chainer関数の使用）
     model = ConvNet()
     optimizer = optimizers.Adam(learing_rate)
     optimizer.setup(model)
@@ -76,31 +75,25 @@ if __name__ == '__main__':
         cuda.get_device(args.gpu).use()
         model.to_gpu()
 
-    #訓練ループ
+    # 訓練ループ
     loss_log = []
     for epoch in range(num_epochs):
         for i in range(0, num_train, batch_size):
-                 x_batch = xp.asarray(x_train[i:i+batch_size]) # 1->バッチサイズまでのループ
-                 ｃ_batch = xp.asarray(c_train[i:i+batch_size])
-                 y_batch = model(x_batch)
-        
-        #損失関数の計算
-                 loss = F.softmax_cross_entropy(y_batch, c_batch)
-                 model.cleargrads()              #勾配のリセット
-                 loss.backward()                 #重みの更新
-                 optimizer.update()
-        
-        
-                 accuracy = F.accuracy(y_batch, c_batch)       #認識率
-    
-                 print(epoch, accuracy.data, loss.data)          #認識率の表示
-                 loss_log.append(loss.data)                      #loss.dataにダータを追加
-                 
-    #グラフの表示
-    plt.plot(loss_log)      
+            x_batch = xp.asarray(x_train[i:i+batch_size])  # 1->バッチサイズまでのループ
+            ｃ_batch = xp.asarray(c_train[i:i+batch_size])
+            y_batch = model(x_batch)
+
+            # 損失関数の計算
+            loss = F.softmax_cross_entropy(y_batch, c_batch)
+            model.cleargrads()              # 勾配のリセット
+            loss.backward()                 # 重みの更新
+            optimizer.update()
+
+            accuracy = F.accuracy(y_batch, c_batch)       # 認識率
+
+            print(epoch, accuracy.data, loss.data)          # 認識率の表示
+            loss_log.append(cuda.to_cpu(loss.data))         # loss.logにデータを追加
+
+    # グラフの表示
+    plt.plot(loss_log)
     plt.show()
-
-
-
-    
-    
