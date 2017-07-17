a75a5072edc7338712ddab2355623d587118a0ec
Matsui <rr0055xx@ed.ritsumei.ac.jp>
2017-07-17 16:52:07 +0900

    images, labelsの関数を集約,ndim = 1, 2の有効化,np.loadによる2回目以降の実行速度高速化

--- dataset/minist_lorder.py
+++ dataset/minist_lorder.py
@@ -14,110 +14,104 @@ import gzip
 
 
 # make cashe for train images
-def make_train_images_np():
-    train_images = 'train-images-idx3-ubyte.gz'
+def make_images_np(file_name):
+    train_images_file = 'train-images-idx3-ubyte.gz'
+    test_images_file = 't10k-images-idx3-ubyte.gz'
 
-    with gzip.open(train_images, 'rb') as f:
-        data = f.read()
+    if file_name == 'train_images':
+        with gzip.open(train_images_file, 'rb') as f:
+            data = f.read()
 
-    num_images = int.from_bytes(data[4:8], 'big')
-    width = int.from_bytes(data[8:12], 'big')
-    height = int.from_bytes(data[12:16], 'big')
-    pixels = np.frombuffer(data, np.uint8, -1, 16)
+        num_images = int.from_bytes(data[4:8], 'big')
+        width = int.from_bytes(data[8:12], 'big')
+        height = int.from_bytes(data[12:16], 'big')
+        pixels = np.frombuffer(data, np.uint8, -1, 16)
+        # Reshape(60000, 28 , 28, 1)
+        images = pixels.reshape(num_images, width, height, 1)
+        np.save('train_images.npy', images)  # ndarrayをファイルに保存する
+        print('Making cashe for train images is completed.')
 
-    # Reshape(60000, 28 , 28, 1)
-    images = pixels.reshape(num_images, width, height, 1)
-    np.save('train_images.npy', images)  # ndarrayをファイルに保存する
+    elif file_name == 'test_images':
+        with gzip.open(test_images_file, 'rb') as f:
+            data = f.read()
 
-    print('Making cashe for train images is completed.')
-    return images
+        num_images = int.from_bytes(data[4:8], 'big')
+        width = int.from_bytes(data[8:12], 'big')
+        height = int.from_bytes(data[12:16], 'big')
+        pixels = np.frombuffer(data, np.uint8, -1, 16)
+        # Reshape(60000, 28 , 28, 1)
+        images = pixels.reshape(num_images, width, height, 1)
+        np.save('test_images.npy', images)  # ndarrayをファイルに保存する
+        print('Making cashe for test images is completed.')
 
-
-# make cashe for test images
-def make_test_images_np():
-    test_images = 't10k-images-idx3-ubyte.gz'
-
-    with gzip.open(test_images, 'rb') as f:
-        data = f.read()
-
-    num_images = int.from_bytes(data[4:8], 'big')
-    width = int.from_bytes(data[8:12], 'big')
-    height = int.from_bytes(data[12:16], 'big')
-    pixels = np.frombuffer(data, np.uint8, -1, 16)
-
-    # Reshape(10000, 28 , 28, 1)
-    images = pixels.reshape(num_images, width, height, 1)
-    np.save('test_images.npy', images)  # ndarrayをファイルに保存する
-
-    print('Making cashe for test images is completed.')
     return images
 
 
 # make cashe for train labels
-def make_train_labeles_np():
-    train_labels = 'train-labels-idx1-ubyte.gz'
+def make_labeles_np(file_name):
+    train_labels_file = 'train-labels-idx1-ubyte.gz'
+    test_labels_file = 't10k-labels-idx1-ubyte.gz'
 
-    with gzip.open(train_labels, 'rb') as f:
-        data = f.read()
+    if file_name == 'train_labels':
+        with gzip.open(train_labels_file, 'rb') as f:
+            data = f.read()
 
-    labels = np.frombuffer(data, np.uint8, -1, 8)
-    np.save('train_labels.npy', labels)  # ndarrayをファイルに保存する
+        labels = np.frombuffer(data, np.uint8, -1, 8)
+        np.save('train_labels.npy', labels)  # ndarrayをファイルに保存する
 
-    print('Making cashe for train labels is completed.')
-    return labels
+        print('Making cashe for train labels is completed.')
 
+    elif file_name == 'test_labels':
+        with gzip.open(test_labels_file, 'rb') as f:
+            data = f.read()
 
-# make cashe for test labels
-def make_test_labeles_np():
-    test_labels = 't10k-labels-idx1-ubyte.gz'
-    with gzip.open(test_labels, 'rb') as f:
-        data = f.read()
+        labels = np.frombuffer(data, np.uint8, -1, 8)
+        np.save('test_labels.npy', labels)  # ndarrayをファイルに保存する
 
-    labels = np.frombuffer(data, np.uint8, -1, 8)
-    np.save('test_labels.npy', labels)  # ndarrayをファイルに保存する
+        print('Making cashe for test labels is completed.')
 
-    print('Making cashe for test labels is completed.')
     return labels
 
 
 # データが残っていたら使用，残っていなかったらキャッシュ作成
-def MnistLoader(ndim=2):
+def mnist_loader(ndim=2):
     root_url = 'http://yann.lecun.com/exdb/mnist'
+
     if not Path('train_images.npy').exists():
         urllib.request.urlretrieve(root_url + '/train-images-idx3-ubyte.gz',
                                    'train-images-idx3-ubyte.gz')  # データファイルのDL
-        train_images_data = make_train_images_np()
+        train_images_data = make_images_np('train_images')
     else:
-        train_images_data = train_images.npy
+        train_images_data = np.load('train_images.npy')
 
     if not Path('test_images.npy').exists():
         urllib.request.urlretrieve(root_url + '/t10k-images-idx3-ubyte.gz',
                                    't10k-images-idx3-ubyte.gz')  # データファイルのDL
-        test_images_data = make_test_images_np()
+        test_images_data = make_images_np('test_images')
     else:
-        test_images_data = test_images.npy
+        test_images_data = np.load('test_images.npy')
 
     if not Path('train_labels.npy').exists():
         urllib.request.urlretrieve(root_url + '/train-labels-idx1-ubyte.gz',
                                    'train-labels-idx1-ubyte.gz')   # データファイルのDL
-        train_labels_data = make_train_labeles_np()
+        train_labels_data = make_labeles_np('train_labels')
     else:
-        train_labels_data = train_labels.npy
+        train_labels_data = np.load('train_labels.npy')
 
     if not Path('test_labels.npy').exists():
         urllib.request.urlretrieve(root_url + '/t10k-labels-idx1-ubyte.gz',
                                    't10k-labels-idx1-ubyte.gz')   # データファイルのDL
-        test_labels_data = make_test_labeles_np()
+        test_labels_data = make_labeles_np('test_labels')
     else:
-        test_labels_data = test_labels.npy
+        test_labels_data = np.load('test_labels.npy')
 
     if ndim == 1:
-        train_images_data = train_images_data.reshape(1, 28 * 28)
-        test_images_data = test_images_data.reshape(1, 28 * 28)
+        train_images_data = train_images_data.reshape(-1, 28 * 28)
+        test_images_data = test_images_data.reshape(-1, 28 * 28)
 
     elif ndim == 2:
-        train_images_data = train_images_data.reshape(1, 28, 28)
-        test_images_data = test_images_data.reshape(1, 28, 28)
+        train_images_data = train_images_data.reshape(-1, 28, 28)
+        test_images_data = test_images_data.reshape(-1, 28, 28)
 
     elif ndim == 3:
         train_images_data = train_images_data.reshape(-1, 1, 28, 28)
@@ -130,10 +124,12 @@ def MnistLoader(ndim=2):
 
 
 if __name__ == '__main__':
-    train_images, test_images, train_labels, test_labels = MnistLoader(3)
-    plt.matshow(train_images[0][0], cmap=plt.cm.gray)
+    train_images, test_images, train_labels, test_labels = mnist_loader(2)
+    a = train_images[0]
+    b = test_images[0]
+    plt.matshow(a, cmap=plt.cm.gray)
     plt.show()
-    plt.matshow(test_images[0][0], cmap=plt.cm.gray)
+    plt.matshow(b, cmap=plt.cm.gray)
     plt.show()
     print('train label = {}'. format(train_labels[0]))
     print('test label = {}'. format(test_labels[0]))
