c7b529bd67a04d35522b39c7dd01b47804d57974
matsui <rr0055xx@ed.ritsumei.ac.jp>
2017-08-02 18:21:24 +0900

    細かい修正

--- Segnet/lorder.py
+++ Segnet/lorder.py
@@ -35,33 +35,32 @@ def CamVid_loader(dataset_dir=root_dir):
     train = TransformDataset(train, transform)
     test = CamVidDataset(dataset_dir, split='val')
 
-"""
+    """
     train_images = np.array(train[:, 0])
     train_classes = np.array(train[:, 1])
     test_images = np.array(test[:, 0])
     test_classes = np.array(test[:, 1])
-"""
-
+    """
     train_images = train[:, 0]
-    train_classes = train[:, 1]
+    train_labels = train[:, 1]
     test_images = test[:, 0]
-    test_classes = test[:, 1]
+    test_labels = test[:, 1]
 
     train_images /= 255.0
-    train_classes /= 255.0
+    train_labels /= 255.0
     test_images /= 255.0
-    test_classes /= 255.0
+    test_labels /= 255.0
 
     return train_images, test_images, train_labels, test_labels
 
 
 if __name__ == '__main__':
     (train_images, test_images,
-     train_labels, test_labels) = np.(CamVid_loader())
+     train_labels, test_labels) = np.array(CamVid_loader())
 
-    a = train_images.transpose(train_images[0](1, 2, 0))
+    a = train_images[0].transpose(1, 2, 0)
     b = train_labels[0]
-    c = test_images.transepose(test_images[0](1, 2, 0))
+    c = test_images[0].transepose(1, 2, 0)
     d = train_labels[0]
 
     plt.imshow(a)
